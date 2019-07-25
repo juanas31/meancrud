5b62129ae8e63bdfe150cacc99fb2b7b931d4a1e
Juanas Smith <juanas@giviews.com>
2019-07-25 22:42:13 +0700

    penambahan fungsi crud

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose');
+
+const app = express();
+let port = process.env.PORT || 4000;
+
+const server = app.listen(function () {
+    console.log('Listening on port ' + port);
+});
